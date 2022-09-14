a812a3037dcbd8a66c616074ffd0b60aeb8d0718
Ian McPhail <webguyian@gmail.com>
2022-09-14 14:35:29 -0400

    Add Error API route

---
+++ pages/api/error.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  error: string;
+};
+
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(401).json({ error: 'Request failed' });
+}
