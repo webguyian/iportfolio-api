fcb63a041424af7359a87da22a24344c1af994cf
Ian McPhail <webguyian@gmail.com>
2022-10-03 13:29:52 -0400

    Add genre to Music API route

--- pages/api/music.ts
+++ pages/api/music.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const ENDPOINT = 'https://api.napster.com/v2.2/tracks/top';
+const ENDPOINT = 'https://api.napster.com/v2.2/genres/g.33/tracks/top';
 const API_KEY = process.env.MUSIC_API_KEY;
 
 export default async function handler(
